144aaadc83406683e5f31eecc094a20a86c8c826
saladlemon <wjsehgns11@naver.com>
2023-01-21 20:49:00 +0900

    episode_widget

--- webtoontest/lib/screens/detail_screen.dart
+++ webtoontest/lib/screens/detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:webtoontest/models/webtoon_detail_model.dart';
 import 'package:webtoontest/models/webtoon_episode_model.dart';
 import 'package:webtoontest/services/api_service.dart';
+import 'package:webtoontest/widgets/episode_widget.dart';
 
 class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
@@ -104,34 +105,7 @@ class _DetailScreenState extends State<DetailScreen> {
                     return Column(
                       children: [
                         for (var episode in snapshot.data!)
-                          Container(
-                            margin: const EdgeInsets.only(bottom: 10),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.green.shade400,
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 10, horizontal: 20),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    episode.title,
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                  const Icon(
-                                    Icons.chevron_right_rounded,
-                                    color: Colors.white,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
+                          Episode(episode: episode),
                       ],
                     );
                   }
---
+++ webtoontest/lib/widgets/episode_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import 'package:webtoontest/models/webtoon_episode_model.dart';
+
+class Episode extends StatelessWidget {
+  const Episode({
+    Key? key,
+    required this.episode,
+  }) : super(key: key);
+
+  final WebtoonEpisodeModel episode;
+
+  onButtonTap() async {
+    await launchUrlString("https://google.com");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.green.shade400,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              episode.title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),
+            ),
+            const Icon(
+              Icons.chevron_right_rounded,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
