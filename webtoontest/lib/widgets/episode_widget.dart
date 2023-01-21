a82509ef61f35f34b011446b5b966fdd02fa8dfb
saladlemon <wjsehgns11@naver.com>
2023-01-21 21:32:55 +0900

    likedToons

--- webtoontest/lib/widgets/episode_widget.dart
+++ webtoontest/lib/widgets/episode_widget.dart
@@ -6,39 +6,45 @@ class Episode extends StatelessWidget {
   const Episode({
     Key? key,
     required this.episode,
+    required this.webtoonID,
   }) : super(key: key);
 
   final WebtoonEpisodeModel episode;
+  final String webtoonID;
 
   onButtonTap() async {
-    await launchUrlString("https://google.com");
+    await launchUrlString(
+        "https://comic.naver.com/webtoon/detail?titleId=$webtoonID&no=${episode.id}");
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.green.shade400,
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              episode.title,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 16,
+    return GestureDetector(
+      onTap: onButtonTap,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.green.shade400,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                episode.title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
               ),
-            ),
-            const Icon(
-              Icons.chevron_right_rounded,
-              color: Colors.white,
-            ),
-          ],
+              const Icon(
+                Icons.chevron_right_rounded,
+                color: Colors.white,
+              ),
+            ],
+          ),
         ),
       ),
     );
