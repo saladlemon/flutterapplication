b47f871bdb71a35b3e82cd48309cc4cbd3cee606
saladlemon <wjsehgns11@naver.com>
2023-01-21 13:49:16 +0900

    Hero

--- webtoontest/lib/widgets/webtoon_widget.dart
+++ webtoontest/lib/widgets/webtoon_widget.dart
@@ -17,46 +17,33 @@ class Webtoon extends StatelessWidget {
       onTap: () {
         Navigator.push(
           context,
-          PageRouteBuilder(
-            transitionsBuilder:
-                (context, animation, secondaryAnimation, child) {
-              var begin = const Offset(1.0, 0.0);
-              var end = Offset.zero;
-              var curve = Curves.ease;
-              var tween =
-                  Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-              return SlideTransition(
-                position: animation.drive(tween),
-                child: child,
-              );
-            },
-            pageBuilder: (context, anmation, secondaryAnimation) =>
-                DetailScreen(id: id, title: title, thumb: thumb),
-          ),
-          /*MaterialPageRoute(
+          MaterialPageRoute(
             builder: (context) => DetailScreen(
               title: title,
               thumb: thumb,
               id: id,
             ),
             fullscreenDialog: true,
-          ),*/
+          ),
         );
       },
       child: Column(
         children: [
-          Container(
-            width: 250,
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                      blurRadius: 3,
-                      offset: const Offset(10, 10),
-                      color: Colors.black.withOpacity(0.5))
-                ]),
-            child: Image.network(thumb),
+          Hero(
+            tag: id,
+            child: Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 3,
+                        offset: const Offset(10, 10),
+                        color: Colors.black.withOpacity(0.5))
+                  ]),
+              child: Image.network(thumb),
+            ),
           ),
           const SizedBox(
             height: 10,
